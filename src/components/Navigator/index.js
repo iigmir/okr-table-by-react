40757d00c668c503c74bca3c032ad6d6c682a9fc
iigmir <roc120j@gmail.com>
2019-07-10 13:46:19 +0800

    ani

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Menu, Image } from "semantic-ui-react";
+import "./index.css";
 
 function Navigator(props)
 {
     return (
     <Menu>
         <Menu.Item>
-            <Image size="mini" src={ props.logo } />
+            <Image className="App-logo" size="mini" src={ props.logo } />
         </Menu.Item>
         <Menu.Item>
             { props.title_name }
