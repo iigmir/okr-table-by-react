486ddb6ef2de33c4954fdd4409ba4763bb013133
iigmir <roc120j@gmail.com>
2019-07-10 17:52:44 +0800

    Set stores

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import React from "react";
 import Main from "./components/Main/index.js";
 import Navigator from "./components/Navigator/index.js";
 import logo from "./logo.svg";
-import { createStore } from "redux";
-import reducer from "./reducers/index.js";
 
-const store = createStore(reducer);
-
-function App() {
-    console.log( store.getState() );
+function App()
+{
     return (
     <div>
         <Navigator logo={logo} title_name="OKR Sheet" />
---
+++ src/redux/Week/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+let Week = ( state = [], action ) =>
+{
+    return state;
+};
+
+export default combineReducers({ Week });
