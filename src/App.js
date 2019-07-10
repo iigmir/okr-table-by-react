accc4c0f8bb9aae5a27d4dcb092469dd242c5648
iigmir <roc120j@gmail.com>
2019-07-10 11:49:03 +0800

    Add navigator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from "react";
-// import Link from "./components/App/Link.js";
-// import ReactLogo from "./components/App/ReactLogo.js";
-// import logo from "./logo.svg";
-import { Rating, Button, Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import "./components/App/App.css";
+import React from "react";
+import Link from "./components/App/Link.js";
+import ReactLogo from "./components/App/ReactLogo.js";
+import Navigator from "./components/Navigator/index.js";
+import logo from "./logo.svg";
 
 function App() {
     return (
-    <div> Hello
-        <Rating rating={1} maxRating={5} />
-        <Button size="small" color="green">
-            <Icon name="download" />
-            Download
-        </Button>
+    <div>
+        <Navigator />
+        <div>
+            <ReactLogo logo={logo} />
+            <Link />
+        </div>
     </div>
-        // <div className="App">
-        //     <ReactLogo logo={logo} />
-        //     <Link />
-        // </div>
     );
 }
 
---
+++ src/components/Navigator/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+function Navigator()
+{
+    return (
+    <Menu>
+        <Menu.Item name="editorials">
+            Editorials
+        </Menu.Item>
+
+        <Menu.Item name="reviews">
+            Reviews
+        </Menu.Item>
+
+        <Menu.Item name="upcomingEvents">
+            Upcoming Events
+        </Menu.Item>
+    </Menu> );
+}
+
+export default Navigator;
