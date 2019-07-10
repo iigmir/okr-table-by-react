4f790ce592a311ba9bd083d9c44982156b7aac6c
iigmir <roc120j@gmail.com>
2019-07-10 15:26:58 +0800

    Prevent map warning

--- src/components/Main/Week.js
+++ src/components/Main/Week.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { List, Form, Input, Button } from "semantic-ui-react";
+import { List, Form } from "semantic-ui-react";
 
-export default props => (
-<div>
-    <List bulleted>
-        <List.Item>Apples</List.Item>
-        <List.Item>Oranges</List.Item>
-        <List.Item>{ props.src }</List.Item>
-    </List>
-    <Form>
-        <Form.Group inline>
-            <Form.Input placeholder="Add weekly target for OKR" />
-            <Form.Button content="Add weekly target" />
-        </Form.Group>
-    </Form>
-</div>
-);
+export default props =>
+{
+    let add_tasks = () => console.log("Submitted");
+    let lists = [ "Apples", "Banana", props.src ];
+    let list_component = lists.map( (item, index) => <List.Item key={index}>{ item }</List.Item> );
+    return (
+        <div>
+            <List bulleted> { list_component } </List>
+            <Form onSubmit={add_tasks}>
+                <Form.Group inline>
+                    <Form.Input placeholder="Add weekly target for OKR" />
+                    <Form.Button content="Add weekly target" />
+                </Form.Group>
+            </Form>
+        </div>
+    )
+};
