005a5b8ec8ff2e3554a1e8be7cd7af5cb8012ee0
iigmir <roc120j@gmail.com>
2019-07-10 11:33:52 +0800

    Add Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import Link from "./components/App/Link.js";
-import ReactLogo from "./components/App/ReactLogo.js";
-import logo from "./logo.svg";
-import "./App.css";
+// import Link from "./components/App/Link.js";
+// import ReactLogo from "./components/App/ReactLogo.js";
+// import logo from "./logo.svg";
+import { Rating, Button, Icon } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./components/App/App.css";
 
 function App() {
     return (
-        <div className="App">
-            <ReactLogo logo={logo} />
-            <Link />
-        </div>
+    <div> Hello
+        <Rating rating={1} maxRating={5} />
+        <Button size="small" color="green">
+            <Icon name="download" />
+            Download
+        </Button>
+    </div>
+        // <div className="App">
+        //     <ReactLogo logo={logo} />
+        //     <Link />
+        // </div>
     );
 }
 
