d234ef04d0062d25602ad602d412bf60372e2ed6
iigmir <roc120j@gmail.com>
2019-07-11 17:28:41 +0800

    Fix disable state

--- src/Main/components/TextInputModal.js
+++ src/Main/components/TextInputModal.js
@@ -7,24 +7,22 @@ class TextInput extends React.Component {
         super(props);
         this.state = {
             awaiting_value: "",
-            type_error: false
+            not_compeleted: true
         };
         this.modal_response_method = props.modal_response_method;
     }
     set_awaiting_value(event)
     {
-        this.setState({ awaiting_value: event.target.value });
+        let awaiting_value = event.target.value.trim();
+        let not_compeleted = awaiting_value.length < 1;
+        this.setState({ awaiting_value, not_compeleted });
     }
     emit_to_parent({ event, bool })
     {
         let content = bool === true ? this.state.awaiting_value : null;
         let payload = { bool, content, event };
-        if( this.state.awaiting_value.trim() !== "" || bool === false )
-        {
-            this.modal_response_method(payload);
-        }
-        // this.setState({ type_error: true });
-        // console.log(this.state.type_error);
+        this.modal_response_method(payload);
+        this.setState({ awaiting_value: "", not_compeleted: true });
     }
     render()
     {
@@ -38,14 +36,24 @@ class TextInput extends React.Component {
             <Modal.Content>
                 <Input
                     placeholder="Awaiting..."
-                    error={ this.state.type_error }
                     value={ this.state.awaiting_value }
                     onChange={ this.set_awaiting_value.bind(this) }
                 />
             </Modal.Content>
             <Modal.Actions>
-                <Button negative onClick={ event => this.emit_to_parent({ event, bool: false }) }> Discard </Button>
-                <Button positive onClick={ event => this.emit_to_parent({ event, bool:  true }) }> Submit </Button>
+                <Button
+                    negative
+                    onClick={ event => this.emit_to_parent({ event, bool: false }) }
+                >
+                    Discard
+                </Button>
+                <Button
+                    positive
+                    onClick={ event => this.emit_to_parent({ event, bool:  true }) }
+                    disabled={ this.state.not_compeleted }
+                >
+                    Submit
+                </Button>
             </Modal.Actions>
         </Modal> );
     }
