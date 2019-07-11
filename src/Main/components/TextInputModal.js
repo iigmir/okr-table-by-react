4751c8290d323ea58267a3dee23ec331f40c03f0
iigmir <roc120j@gmail.com>
2019-07-11 16:22:04 +0800

    Set method

--- src/Main/components/TextInputModal.js
+++ src/Main/components/TextInputModal.js
@@ -5,18 +5,23 @@ class TextInput extends React.Component {
     constructor(props)
     {
         super(props);
-        this.state = {};
+        this.state = {
+            awaiting_value: ""
+        };
         this.modal_response_method = props.modal_response_method;
+    }
+    set_awaiting_value(event)
+    {
+        this.setState({ awaiting_value: event.target.value });
     }
     emit_to_parent({ event, bool })
     {
-        let content = bool === true ? "foobar" : "";
+        let content = bool === true ? this.state.awaiting_value : null;
         let payload = {
             event,
             bool,
             content
         };
-        // debugger;
         this.modal_response_method(payload);
     }
     render()
@@ -25,7 +30,7 @@ class TextInput extends React.Component {
         <Modal basic size="small" open={ this.props.open } closeOnDimmerClick={ false }>
             <Modal.Header>Input what you try to type</Modal.Header>
             <Modal.Content>
-                <Input placeholder="Awaiting..." />
+                <Input ref="input_content" placeholder="Awaiting..." value={ this.state.awaiting_value } onChange={ this.set_awaiting_value.bind(this) } />
             </Modal.Content>
             <Modal.Actions>
                 <Button negative onClick={ event => this.emit_to_parent({ event, bool: false }) }> Discard </Button>
